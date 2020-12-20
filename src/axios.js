ee607bb18cb1955e40af96946f766981b3226538
Justin Chang <ycchang0324@gmail.com>
2020-12-20 13:15:51 +0800

    change server url

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_ROOT = 'http://localhost:5000/api';
+const API_ROOT = 'https://acs-web-final-project.herokuapp.com/api';
 const instance = axios.create({
   baseURL: API_ROOT
 })
