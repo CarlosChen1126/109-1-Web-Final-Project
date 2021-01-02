757bd9c068d07e2eedc2fd5f0b956a5839648644
Justin Chang <ycchang0324@gmail.com>
2021-01-02 20:40:47 +0800

    add favicon into build

--- src/components/Logout.js
+++
@@ -1,9 +0,0 @@
-import { Redirect } from "react-router-dom";
-
-function logout(){
-    localStorage.setItem("auth", false);
-    console.log('there');
-    return <Redirect to="/ManagerLogin" />
-}
-
-export default logout;
